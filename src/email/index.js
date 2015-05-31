da22af099a83f3b856f044459b04dcb05f34c993
Vinh <kureikain@gmail.com>
2015-05-31 12:35:25 -0700

    email and init test

--- src/email/index.js
+++ src/email/index.js
@@ -1,43 +1,26 @@
 var email   = require("emailjs/email");
 var server  = email.server.connect({
-  user:    "username", 
-  password:"password", 
-  host:    "smtp.your-email.com", 
-  ssl:     true
+  user:     process.env.SMTP_USER,
+  password: process.env.SMTP_PASSWORD,
+  host:     process.env.SMTP_HOST,
+  ssl:     true,
+  port:     process.env.SMTP_PORT
 });
 
 var RESDIR = __dirname + "/../../res/";
 
 module.exports = exports = noty = function (opt) {
-  this.resdir = RESDIR
 }
 
-noty.prototype.alarm = function () {
+noty.prototype.alarm = noty.alarm = function () {
 
-  var player = new Player([
-    //RESDIR + 'cotrom.mp3',
-    RESDIR + 'police.mp3',
-    //RESDIR + 'cotrom.mp3'
-  ])
-
-  player.play(function(err, player) {
-    console.log("Play warning sound")		
-  })
-
-  player.on('playing',function(item){
-    console.log('Im playing... src:' + item);
-  });
-
-  // event: on playend 
-  player.on('playend',function(item){
-    // return a playend item 
-    console.log('src:' + item + ' play done, switching to next one ...');
-  });
-
-  // event: on error 
-  player.on('error', function(err){
-    // when error occurs 
-    console.log(err);
-  });
+  // send the message and get a callback with an error or details of the message that was sent
+  server.send({
+    text:    "i hope this works", 
+    from:    "you <k2351987@gmail.com>", 
+    to:      "vinh <vinh@listia.com>, another <kurei@axcoto.com>",
+    cc:      "else <kureikain@gmail.com>",
+    subject: "testing emailjs"
+  }, function(err, message) { console.log(err || message); });
 
 }
---
+++ test/email.js
@@ -0,0 +1,12 @@
+var assert = require("assert"); // node.js core module
+
+var Email = require('../src/email/index')
+var email  = new Email()
+
+describe('Email', function(){
+  describe('#alarm', function(){
+    it('should send email', function(){
+      
+    })
+  })
+})
